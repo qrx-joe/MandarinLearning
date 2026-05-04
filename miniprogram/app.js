4ef890de746ab8bea27848145ecb342a75a99c66
qrx-joe <1453626854@qq.com>
2026-05-04 22:41:34 +0800

    refactor: 关闭云开发，切换为纯本地模式

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,33 +10,8 @@ App({
   },
 
   onLaunch() {
-    if (wx.cloud) {
-      wx.cloud.init({
-        env: 'mandarin-app-v2-8gby81rpf2d67f6b',
-        traceUser: true
-      })
-
-      // 探测云环境是否真正可用
-      wx.cloud.callFunction({
-        name: 'login',
-        success: (res) => {
-          this.globalData.cloudReady = true
-          console.log('✅ 云开发环境正常，openid:', res.result.openid)
-          if (res.result.openid) {
-            wx.setStorageSync('openid', res.result.openid)
-            this.globalData.openid = res.result.openid
-          }
-        },
-        fail: (err) => {
-          this.globalData.cloudReady = false
-          console.error('❌ 云开发环境不可用:', err.errMsg || err.message)
-        }
-      })
-    } else {
-      this.globalData.cloudReady = false
-      console.log('❌ 当前基础库不支持云开发')
-    }
-
+    // 云开发已关闭，使用本地模式
+    this.globalData.cloudReady = false
     this.checkLoginStatus()
   },
 
